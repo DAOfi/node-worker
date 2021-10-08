037333aa4d3e0679ca0fbac54c1029866563b7ab
Alex Lewis <alexvlewis@gmail.com>
2021-10-08 16:17:53 -0700

    Cleanup infra files

--- src/utils.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-export async function getGasPrice() {
-  return (
-    await axios.get(
-      `https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=${process.env.ETHERSCAN_KEY}`
-    )
-  ).data.result.FastGasPrice // ProposeGasPrice
-}
